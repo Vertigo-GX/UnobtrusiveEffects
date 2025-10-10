ea2a898f551785bf9629f5fb44eead9b4a655aea
Vertigo GX <vertigo_gx@protonmail.com>
2025-10-10 21:02:21 +0200

    Update to 1.21.9

---
+++ src/client/java/vertigo/unobtrusiveeffects/mixin/client/BillboardParticleMixin.java
@@ -0,0 +1,30 @@
+package vertigo.unobtrusiveeffects.mixin.client;
+
+import net.minecraft.client.particle.BillboardParticle;
+import net.minecraft.client.particle.BillboardParticleSubmittable;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vertigo.unobtrusiveeffects.UnobtrusiveEffectsClient;
+
+@Mixin(BillboardParticle.class)
+public abstract class BillboardParticleMixin extends Particle {
+
+	protected BillboardParticleMixin(ClientWorld world, double x, double y, double z) {
+		super(world, x, y, z);
+	}
+
+	@Inject(method = "render(Lnet/minecraft/client/particle/BillboardParticleSubmittable;Lnet/minecraft/client/render/Camera;F)V", at = @At("HEAD"),
+			cancellable = true)
+	public void renderInject(BillboardParticleSubmittable submittable, Camera camera, float tickProgress, CallbackInfo info) {
+		if(UnobtrusiveEffectsClient.CONFIG.swirlCullingDistance != 0 && !camera.isThirdPerson() && camera.getPos().squaredDistanceTo(this.x, this.y,
+				this.z) < UnobtrusiveEffectsClient.CONFIG.swirlCullingDistance) {
+			info.cancel();
+		}
+	}
+
+}
--- src/client/java/vertigo/unobtrusiveeffects/mixin/client/InGameOverlayRendererMixin.java
+++ src/client/java/vertigo/unobtrusiveeffects/mixin/client/InGameOverlayRendererMixin.java
@@ -3,6 +3,7 @@ package vertigo.unobtrusiveeffects.mixin.client;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +16,9 @@ import vertigo.unobtrusiveeffects.UnobtrusiveEffectsClient;
 public abstract class InGameOverlayRendererMixin {
 
 	@Inject(method = "renderFireOverlay", at = @At("HEAD"), cancellable = true)
-	private static void renderFireOverlayInject(MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo info) {
-		if (UnobtrusiveEffectsClient.CONFIG.disableFireOverlay && MinecraftClient.getInstance().player.hasStatusEffect(StatusEffects.FIRE_RESISTANCE)) {
+	private static void renderFireOverlayInject(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Sprite sprite, CallbackInfo info) {
+		if(UnobtrusiveEffectsClient.CONFIG.disableFireOverlay && MinecraftClient.getInstance().player.hasStatusEffect(
+				StatusEffects.FIRE_RESISTANCE)) {
 			info.cancel();
 		}
 	}
